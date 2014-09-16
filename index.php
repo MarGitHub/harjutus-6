395e647adf9759e11a9faeb4a16bd7417cfdc2bd
Margit <margit.pilden@gmail.com>
2014-09-16 13:23:47 +0300

    Created basic html

---
+++ index.php
@@ -0,0 +1,14 @@
+<! DOCKTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>
+	harjtus-3s - PHP põhitõed
+	</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
